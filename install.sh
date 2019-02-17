d81cc417ac50292fc7fb1f50b905849db135c729
kylinfedora <kylin_fedora@hotmail.com>
2019-02-17 16:53:23 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,44 +8,49 @@
 #######################################################################
 
 #User name
-echo Enter your current username:
-read usern
+#echo Enter your current username:
+#read usern
 
 #Domain name
-echo Enter your duckdns domain name e.g. test.duckdns.org:
-read domain
+#echo Enter your duckdns domain name e.g. test.duckdns.org:
+#read domain
 
 #Duckdns token
-echo Enter your duckdns token:
-read token
+#echo Enter your duckdns token:
+#read token
 
 #Letsencrypt challenge 
-echo Which challenge should be used? Currently http-01 and dns-01 are supported
-oldIFS=$IFS
-IFS=$'\n'
-choices=( http-01 dns-01 )
-IFS=$oldIFS
-PS3="Please enter your choice: "
-select answer in "${choices[@]}"; do
-  for item in "${choices[@]}"; do
-    if [[ $item == $answer ]]; then
-      break 2
-    fi
-  done
-done
+#echo Which challenge should be used? Currently http-01 and dns-01 are supported
+#oldIFS=$IFS
+#IFS=$'\n'
+#choices=( http-01 dns-01 )
+#IFS=$oldIFS
+#PS3="Please enter your choice: "
+#select answer in "${choices[@]}"; do
+#  for item in "${choices[@]}"; do
+#    if [[ $item == $answer ]]; then
+#      break 2
+#    fi
+#  done
+#done
+usern=homeassistant
 
 #Up-to-date
-sudo apt-get update
-sudo apt-get upgrade -y
+apt-get update
+apt-get upgrade -y
 
 #Install prerequisites
-sudo apt-get install python3-pip python3-venv -y
+apt-get install python3-pip python3-venv -y
 
+su -s /bin/bash homeassistant
+cd /home/"$usern"/homeassistant
 #Setup virtual environment
 python3 -m venv homeassistant
 
 source /home/"$usern"/homeassistant/bin/activate
-python3 -m pip install wheel
+
+python3 -m pip install --upgrade pip setuptools
+python3 -m pip install wheel netdisco warrant
 
 #Install Home Assistant
 python3 -m pip install homeassistant
@@ -74,63 +79,63 @@ sudo bash -c 'systemctl start homeassistant@'"$usern"'.service'
 #Begin duckdns letsencrypt
 
 #Prerequisites
-sudo rm -r /home/"$usern"/dehydrated
+#sudo rm -r /home/"$usern"/dehydrated
 
 #Download prerequisites
-git clone https://github.com/lukas2511/dehydrated.git /home/"$usern"/dehydrated
+#git clone https://github.com/lukas2511/dehydrated.git /home/"$usern"/dehydrated
 
 #Setup dehydrated
-touch /home/"$usern"/dehydrated/domains.txt
-cat >> /home/"$usern"/dehydrated/domains.txt << EOF
-$domain
-EOF
-cp /home/"$usern"/build_hassio/config /home/"$usern"/dehydrated/
-sudo sed -i -e "s/answer/$answer/g" /home/"$usern"/dehydrated/config
+#touch /home/"$usern"/dehydrated/domains.txt
+#cat >> /home/"$usern"/dehydrated/domains.txt << EOF
+#$domain
+#EOF
+#cp /home/"$usern"/build_hassio/config /home/"$usern"/dehydrated/
+#sudo sed -i -e "s/answer/$answer/g" /home/"$usern"/dehydrated/config
 
 #Setup hook.sh
-cp /home/"$usern"/build_hassio/hook.sh /home/"$usern"/dehydrated/
-sudo sed -i -e "s/ind/$domain/g" /home/"$usern"/dehydrated/hook.sh
-sudo sed -i -e "s/int/$token/g" /home/"$usern"/dehydrated/hook.sh
-sudo sed -i -e "s/usern/$usern/g" /home/"$usern"/dehydrated/hook.sh
-chmod 755 /home/"$usern"/dehydrated/hook.sh
+#cp /home/"$usern"/build_hassio/hook.sh /home/"$usern"/dehydrated/
+#sudo sed -i -e "s/ind/$domain/g" /home/"$usern"/dehydrated/hook.sh
+#sudo sed -i -e "s/int/$token/g" /home/"$usern"/dehydrated/hook.sh
+#sudo sed -i -e "s/usern/$usern/g" /home/"$usern"/dehydrated/hook.sh
+#chmod 755 /home/"$usern"/dehydrated/hook.sh
 
 #Generate certificate
-cd /home/"$usern"/dehydrated
-./dehydrated --register  --accept-terms
-./dehydrated -c
+#cd /home/"$usern"/dehydrated
+#./dehydrated --register  --accept-terms
+#./dehydrated -c
 
 #Add certificate to home assistant
-cert=$(echo $(sudo find /home/admin/dehydrated/certs/ -name "fullchain.pem"))
-key=$(echo $(sudo find /home/admin/dehydrated/certs/ -name "privkey.pem*"))
+#cert=$(echo $(sudo find /home/admin/dehydrated/certs/ -name "fullchain.pem"))
+#key=$(echo $(sudo find /home/admin/dehydrated/certs/ -name "privkey.pem*"))
 
-sed -i '/^http\:/a \ \ base_url\: '"$domain"':8123' /home/"$usern"/.homeassistant/configuration.yaml
-sed -i '/^http\:/a \ \ ssl_certificate\: '"$cert"'' /home/"$usern"/.homeassistant/configuration.yaml
+#sed -i '/^http\:/a \ \ base_url\: '"$domain"':8123' /home/"$usern"/.homeassistant/configuration.yaml
+#sed -i '/^http\:/a \ \ ssl_certificate\: '"$cert"'' /home/"$usern"/.homeassistant/configuration.yaml
 sed -i '/^http\:/a \ \ ssl_key\: '"$key"'' /home/"$usern"/.homeassistant/configuration.yaml
 
 #Restart home assistant
-sudo bash -c 'sudo systemctl restart homeassistant@'"$usern"'.service'
+#sudo bash -c 'sudo systemctl restart homeassistant@'"$usern"'.service'
 
 #Setup duckdns cron
-rm -r /home/"$usern"/duckdns
-mkdir /home/"$usern"/duckdns
-subd=$(echo $domain | sed 's/[.].*$//')
-touch /home/"$usern"/duckdns/duck.sh
-cat >> /home/"$usern"/duckdns/duck.sh << EOF
-echo url="https://www.duckdns.org/update?domains=$subd&token=$token&ip=" | curl -k -o /home/"$usern"/duckdns/duck.log -K -
-EOF
-chmod 700 /home/"$usern"/duckdns/duck.sh
-sudo /home/"$usern"/duckdns/duck.sh
+#rm -r /home/"$usern"/duckdns
+#mkdir /home/"$usern"/duckdns
+#subd=$(echo $domain | sed 's/[.].*$//')
+#touch /home/"$usern"/duckdns/duck.sh
+#cat >> /home/"$usern"/duckdns/duck.sh << EOF
+#echo url="https://www.duckdns.org/update?domains=$subd&token=$token&ip=" | curl -k -o /home/"$usern"/duckdns/duck.log -K -
+#EOF
+#chmod 700 /home/"$usern"/duckdns/duck.sh
+#sudo /home/"$usern"/duckdns/duck.sh
 
 #Add crontabs
-crontab -l > mycron
-echo "0 1 1 * * /home/$usern/dehydrated/dehydrated -c" >> mycron
-crontab mycron
-rm mycron
+#crontab -l > mycron
+#echo "0 1 1 * * /home/$usern/dehydrated/dehydrated -c" >> mycron
+#crontab mycron
+#rm mycron
 
-crontab -l > mycron
-echo "*/5 * * * * /home/"$usern"/duckdns/duck.sh >/dev/null 2>&1" >> mycron
-crontab mycron
-rm mycron
+#crontab -l > mycron
+#echo "*/5 * * * * /home/"$usern"/duckdns/duck.sh >/dev/null 2>&1" >> mycron
+#crontab mycron
+#rm mycron
 
 #Clean up
 clear
